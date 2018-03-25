2e523533fe14c01fa91dcb8b22f579213df18400
ghj3 <k3sekido@gmail.com>
2018-03-25 11:46:31 +0900

    delete method

--- guessmynum.py
+++ guessmynum.py
@@ -5,13 +5,11 @@ Created on Sun Mar 25 10:21:29 2018
 
 @author: k3sekido
 """
-def bisearch(st,ed):
-    return int((ed + st) / 2)
 st=0
 ed=100
 print("Please think of a number between 0 and 100!")
 while True:
-    hp = bisearch(st,ed)
+    hp = int((ed + st) / 2)
     print("Is your secret number " + str(hp) + "?")
     ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
     if ans =='h':
